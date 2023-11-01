0c7ab531abaee82b7461b15487bd315ea9512a38
iBhokary-dev <Bhokary404@proton.me>
2023-11-01 04:12:41 -0300

    Update: - New starships's profiles - New .ps1 profile with loading optmization - New terminal profile with cask. code font

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,19 +1,13 @@
-$ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
-$ENV:STARSHIP_DISTRO = "者 xcad"
-Invoke-Expression (&starship init powershell)
-Import-Module Terminal-Icons
+function Show-TerminalIcons {
+    Import-Module Terminal-Icons
+    Show-TerminalIcons @args
+}
 
-# Enable I-M 
-# $key = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Console\TrueTypeFont'
-# ∙ Set-ItemProperty -Path $key -Name '000' -Value 'FiraCode NFM Retina'
-# ∙
+function Initialize-Profile {
+    $ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
+    Invoke-Expression (&starship init powershell)
+}
 
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-#$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-#if (Test-Path($ChocolateyProfile)) {
-#  Import-Module "$ChocolateyProfile"
-#}
+if ($host.Name -eq "ConsoleHost") {
+    Initialize-Profile
+}
