6d1d4116015decaafa558c78a00945ee3e114924
iBhokary-dev <Bhokary404@proton.me>
2024-01-10 21:10:29 -0300

    add zoixide on profile

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 $ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
 $ENV:STARSHIP_DISTRO = "󰍲"
+
 # Load the Terminal icons module.
 Import-Module -Name Terminal-Icons
 
@@ -8,6 +9,10 @@ Import-Module -Name PSReadLine
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows 
+Set-PSReadLineOption -Colors @{ InlinePrediction = $PSStyle.Background.Blue }
+
+# Load the Zoxide module.
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
 # Load the Starship prompt.
 Invoke-Expression (&starship init powershell)
